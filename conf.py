77d2271317df0d832a939dd9030cfc0b88d0d875
Darihll <98134729+Darihll@users.noreply.github.com>
2022-12-18 21:53:31 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,3 +1,3 @@
 api_hash = "9d8097d05bbc90a6ed2a7a81abcd4e8a"
 api_id = 9902519
-bot_token = "5457176222:AAF09rSPQ8YdmNnjGwMcpsPgl15sEa9zmsI"
+bot_token = "5762258841:AAF_T2lW812AVyqqdFeYoMh96-qA5kn0Wzo"
